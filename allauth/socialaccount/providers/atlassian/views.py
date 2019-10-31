d279fe8591401f9d0e305fe3543f4e74bf1e0dab
mgautam80 <mgautam@gmail.com>
2019-10-31 16:05:55 -0500

    space issue

--- allauth/socialaccount/providers/atlassian/views.py
+++ allauth/socialaccount/providers/atlassian/views.py
@@ -22,8 +22,7 @@ class AtlassianAuth2Adapter(OAuth2Adapter):
         resp = requests.get(self.profile_url, params={'access_token': token.token})
 
         extra_data = resp.json()['data']
-        return self.get_provider().sociallogin_from_response(request,
-                                                             extra_data)
+        return self.get_provider().sociallogin_from_response(request, extra_data)
         # extra_data = resp.json()
         # if app_settings.QUERY_EMAIL and not extra_data.get('email'):
         #     extra_data['email'] = self.get_email(token)
